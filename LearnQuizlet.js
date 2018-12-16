45f77a549e9a771e4db48ccb3677ff32c270a048
brian <br.3cow@gmail.com>
2018-12-16 08:19:23 -0500

    Add files via upload

---
+++ LearnQuizlet.js
@@ -0,0 +1,1 @@
+function LearnModeFindQA (){ var x = 0; while (x<Quizlet.assistantModeData.terms.length){console.log("Term: " + Quizlet.assistantModeData.terms[x].word + " , Definition: " + Quizlet.assistantModeData.terms[x].definition); x++} return(true)}
