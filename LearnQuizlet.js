7045a324b0a62ae85cb499be7eb686b33f96b4b6
brian <br.3cow@gmail.com>
2018-12-16 08:19:52 -0500

    Update LearnQuizlet.js

--- LearnQuizlet.js
+++ LearnQuizlet.js
@@ -1,1 +1,1 @@
-function LearnModeFindQA (){ var x = 0; while (x<Quizlet.assistantModeData.terms.length){console.log("Term: " + Quizlet.assistantModeData.terms[x].word + " , Definition: " + Quizlet.assistantModeData.terms[x].definition); x++} return(true)}
+function LearnModeFindQA (){ /*Codes by Brian*/var x = 0; while (x<Quizlet.assistantModeData.terms.length){console.log("Term: " + Quizlet.assistantModeData.terms[x].word + " , Definition: " + Quizlet.assistantModeData.terms[x].definition); x++} return(true)}
